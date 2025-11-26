7e104a9aae5204619a2c88b2120591c274b797bd
Ankit Yadav <ankrk1111@gmail.com>
2025-11-26 17:51:30 +0530

    Added Q2 program

---
+++ Variables/Q2.java
@@ -0,0 +1,9 @@
+package Variables;
+
+public class Q2 {
+    public static void main(String[] args){
+        System.out.println("My name is Ankit yadav.");
+        System.out.println("I am 22 years old.");
+        System.out.println("I live in Jaunpur city");
+    }   
+}
