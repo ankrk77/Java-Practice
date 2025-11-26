20890b1b4dbe1097821fd453473afd967cd7ff2d
Ankit Yadav <ankrk1111@gmail.com>
2025-11-26 18:42:40 +0530

    Added Q7 .....

--- Variables/Q7.java
+++ Variables/Q7.java
@@ -1,5 +1,10 @@
 package Variables;
 
 public class Q7 {
-    
+    public static void main(String[] args) {
+        int length = 5;
+        int breadth = 9;
+        int area = length * breadth;
+        System.out.println("The area of Rectangle is "+ area);
+    }
 }
