debd397e839b2b57dcc61a5f671c90d802d91a64
Ankit Yadav <ankrk1111@gmail.com>
2025-11-26 18:25:01 +0530

    Added Q4 ....

---
+++ Variables/Q4.java
@@ -0,0 +1,11 @@
+//Do numbers store karo aur unka sum print karo.
+package Variables;
+
+public class Q4 {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 4;
+        int sum = num1 + num2;
+        System.out.println("The Sum is "+ sum);
+    }
+}
