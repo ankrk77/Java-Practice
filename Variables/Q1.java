5e097cf121d3df8ebdd7a2cce7efc15534ac0fae
Ankit Yadav <ankrk1111@gmail.com>
2025-11-26 17:11:16 +0530

    Added Q1.java program

---
+++ Variables/Q1.java
@@ -0,0 +1,13 @@
+//Create a program and print your Name,Age and City.
+package Variables;
+
+public class Q1 {
+    public static void main(String args[]){
+        String name="Ankit yadav";
+        int age = 22;
+        String city="Jaunpur";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+    }
+}
