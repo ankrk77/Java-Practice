796a95d40d13a71a8f6fd6101604d564ea16b34a
Ankit Yadav <ankrk1111@gmail.com>
2025-11-26 18:32:42 +0530

    Added Q5 .....

---
+++ Variables/Q5.java
@@ -0,0 +1,8 @@
+package Variables;
+
+public class Q5 {
+    public static void main(String[] args) {
+        float temp = 35;
+        System.out.println("Tamperature is "+ temp +" degree (Celsius)");
+    }
+}
