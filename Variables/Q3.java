849ba36091041ec81fc540e5b9f88e4613a38806
Ankit Yadav <ankrk1111@gmail.com>
2025-11-26 18:04:43 +0530

    Added Q3 ...

---
+++ Variables/Q3.java
@@ -0,0 +1,11 @@
+package Variables;
+
+public class Q3 {
+    public static void main(String[] args){
+        System.out.println("1.Bahubali");
+        System.out.println("2.Ek Diwane Ki Deewaniyat");
+        System.out.println("3.Jolly LLB");
+        System.out.println("4.Veer-Zaraa");
+        System.out.println("5.Padmavat");
+    }
+}
