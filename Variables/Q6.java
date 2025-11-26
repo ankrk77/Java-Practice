1b70754f88c085781f9f40d666025320329e5fe7
Ankit Yadav <ankrk1111@gmail.com>
2025-11-26 18:37:06 +0530

    Added Q6 .....

--- Variables/Q6.java
+++ Variables/Q6.java
@@ -1,5 +1,9 @@
 package Variables;
 
 public class Q6 {
-    
+    public static void main(String[] args) {
+        String name= "Aditi";
+        int marks = 99;
+        System.out.println("The marks of "+ name +" is "+marks );
+    }
 }
