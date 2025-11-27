c39069860b0c59204041edf87325e2a873e978e5
Ankit Yadav <ankrk1111@gmail.com>
2025-11-28 00:06:33 +0530

    Added Q1

---
+++ Loops/Q1.java
@@ -0,0 +1,10 @@
+//1 to 10 print karo
+package Loops;
+
+public class Q1 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
