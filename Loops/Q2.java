0d55ee6403a9a4f5cad389414063465f4de8bf48
Ankit Yadav <ankrk1111@gmail.com>
2025-11-28 00:10:24 +0530

    Added Q2.....

---
+++ Loops/Q2.java
@@ -0,0 +1,13 @@
+//1 to 100 tak even numbers print karo
+package Loops;
+
+public class Q2 {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
