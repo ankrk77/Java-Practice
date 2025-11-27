4ab9227175a563ada4c835b5f0f46b4758346b69
Ankit Yadav <ankrk1111@gmail.com>
2025-11-28 00:18:50 +0530

    Added Q4....

---
+++ Loops/Q4.java
@@ -0,0 +1,10 @@
+//10 to 1 reverse me print karo
+package Loops;
+
+public class Q4 {
+    public static void main(String[] args) {
+        for(int i=10; i>=1; i--){
+                System.out.println(i);
+        }
+    }
+}
