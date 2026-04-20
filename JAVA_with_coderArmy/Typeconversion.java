0c81309b01b2da4262fe8fbb78037961eb391ea8
Ankit <ankrk77@gmail.com>
2026-04-20 17:26:42 +0530

    java new learning folder added

---
+++ JAVA_with_coderArmy/Typeconversion.java
@@ -0,0 +1,24 @@
+public class Typeconversion {
+    public static void main(String[] agrs) {
+        // Implicit convertion
+        // byte to int
+        // byte b = 24;
+        // int i;
+        // i = b;
+        // System.out.println(i);
+
+        // character to int
+        // char c = 'a';
+        // int i;
+        // i = c;
+        // System.out.println(i); //97
+
+        // Explicit convertion
+
+        int i = 300;
+        byte a;
+        a = (byte) i;
+        System.out.println(a);
+
+    }
+}
